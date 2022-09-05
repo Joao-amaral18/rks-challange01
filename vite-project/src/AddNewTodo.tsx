08e15dc4fdc0e38d85aeeab751723a1da016af7e
Joao-amaral18 <joaovitor.lima.amaral@gmail.com>
2022-09-05 20:01:51 -0300

    adapitacao

--- vite-project/src/AddNewTodo.tsx
+++ vite-project/src/AddNewTodo.tsx
@@ -5,9 +5,12 @@ import "./AddNewTodo.css";
 import ToDo from "./ToDo";
 
 function AddNewTodo() {
-
-  const [tasks, setTasks]: any[] = useState([])
-  const [newTask, setNewTask]: any[] = useState('')
+  const [tasks, setTasks]: any[] = useState([]);
+  const [newTask, setNewTask]: any[] = useState("");
+  const [newChicken, setNewChicken]: any[] = useState("");
+  const [newChickenLegs, setNewChickenLegs]: any[] = useState("");
+  const [newBeans, setNewBeans]: any[] = useState("");
+  const [newSalp, setNewSalp]: any[] = useState("");
 
   function handleCreateNewTodo() {
     event?.preventDefault();
@@ -17,41 +20,103 @@ function AddNewTodo() {
         id: uuidv4(),
         isFinished: false,
         title: newTask,
+        chicken: newChicken,
+        chickenLegs: newChickenLegs,
+        beans: newBeans,
+        salp: newSalp,
       },
     ]);
-    setNewTask('')
+    setNewTask("");
+    setNewChicken("");
+    setNewChickenLegs("");
+    setNewBeans("");
+    setNewSalp("");
   }
-  function handleNewTaskChange($event: any){
-    setNewTask($event?.target.value)
+  function handleNewTaskChange($event: any) {
+    setNewTask($event?.target.value);
+  }
+  function handleNewChickenChange($event: any) {
+    setNewChicken($event?.target.value);
+  }
+  function handleNewBeansChange($event: any) {
+    setNewBeans($event?.target.value);
+  }
+  function handleNewSalpChange($event: any) {
+    setNewSalp($event?.target.value);
+  }
+  function handleNewChickenLegsChange($event: any) {
+    setNewChickenLegs($event?.target.value);
   }
 
-  function deleteTask(id: any){
+  function deleteTask(id: any) {
     const taskToDelete = tasks.filter((task: any) => {
-        console.log(id)
-        return task.id !== id
-    })
-    setTasks(taskToDelete)
+      console.log(id);
+      return task.id !== id;
+    });
+    setTasks(taskToDelete);
   }
-
+  const [totalRemaning, setTotalRemaing] = useState(0)
+  
   return (
     <>
-    <form onSubmit={handleCreateNewTodo} className="newTaskForm">
-      <input name="content" value={newTask} onChange={handleNewTaskChange} type="text" />
-      <button>
-        Criar <PlusCircle size={16} />
-      </button>
-    </form>
-    <div>
-     {tasks.map((val: { id: any; isFinished: any; onDelete: any; title: any; })=> (
-      <ToDo
-        key={val.id}
-        id={val.id}
-        isFinished={val.isFinished}
-        onDelete={deleteTask}
-        title={val.title}
-      />
-    ))}
-    </div>
+      <form onSubmit={handleCreateNewTodo} className="newTaskForm">
+        <input
+          name="content"
+          placeholder="Nome"
+          value={newTask}
+          onChange={handleNewTaskChange}
+          type="text"
+        />
+        <input
+          name="chicken"
+          placeholder="Frango"
+          value={newChicken}
+          onChange={handleNewChickenChange}
+          type="number"
+        />
+        <input
+          name="beans"
+          placeholder="Feijao"
+          value={newBeans}
+          onChange={handleNewBeansChange}
+          type="number"
+        />
+        <input
+          name="salp"
+          placeholder="Salpicao"
+          value={newSalp}
+          onChange={handleNewSalpChange}
+          type="number"
+        />
+        <input
+          name="chickenLegs"
+          placeholder="Coxa"
+          value={newChickenLegs}
+          onChange={handleNewChickenLegsChange}
+          type="number"
+        />
+        <button>
+          Criar <PlusCircle size={16} />
+        </button>
+      </form>
+      <div>
+        {console.log(tasks.length)}
+        {tasks.map(
+          (val: { id: any; isFinished: any; onDelete: any; title: any;chicken: any;chickenLegs: any;beans: any;salp: any }) => (
+            <ToDo
+              key={val.id}
+              id={val.id}
+              isFinished={val.isFinished}
+              onDelete={deleteTask}
+              title={val.title}
+              chicken={val.chicken}
+              chickenLegs={val.chickenLegs}
+              beans={val.beans}
+              salp={val.salp}
+            />
+          )
+        )}
+      </div>
     </>
   );
 }
--- vite-project/src/ToDo.tsx
+++ vite-project/src/ToDo.tsx
@@ -2,7 +2,7 @@ import { TrashSimple } from "phosphor-react"
 import { useState } from "react"
 import './ToDo.css'
 
-function ToDo({title, id, isFinished, onDelete}) {
+function ToDo({title,chicken,salp,chickenLegs,beans, id, isFinished, onDelete}) {
   const [isComplete, setIsComplete] = useState(isFinished)
 
   function handleCompletion(){
@@ -17,9 +17,24 @@ function ToDo({title, id, isFinished, onDelete}) {
     <div className={isComplete ? 'task complete' : 'task'}>
       <div>
       <label>
-        <input className="checkmark" type="checkbox" onClick={handleCompletion}/>
+        <button type="button" className={isComplete ? 'isFinished completeButton' : 'isFinished'} onClick={handleCompletion}>Vendido</button>
       </label>
-      {title}
+      </div>
+      <div>
+
+      <h4>Nome : {title}</h4>
+      
+      
+      <span>{chicken} Frango</span>
+      
+      
+      <span>{chickenLegs} Coxa</span>
+      
+      
+      <span>{beans} Feijão</span>
+      
+      
+      <span>{salp} Salpicão</span>
       </div>
       <div><button className="deleteButton"  onClick={handleDeleteTask}><TrashSimple size={20}/></button></div>
     </div>
